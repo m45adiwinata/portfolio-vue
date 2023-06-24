1d45c450296f5b3721bd6d9b80c07bf250bc1fd7
m45adiwinata <winata@bsa.id>
2023-06-25 03:04:00 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+  import { RouterLink } from 'vue-router'
+  import 'bootstrap/dist/css/bootstrap.css'
+  import Profile from '../src/components/Profile.vue'
+  import Navbar from '../src/components/Navbar.vue'
+</script>
+
+<template>
+  <Navbar />
+  <div class="row">
+    <div class="col-md-4"></div>
+    <div class="col-md-4 text-center">
+      <Profile />
+    </div>
+    <div class="col-md-4"></div>
+  </div>
+  <div class="row">
+    <div class="col-md-12 text-center mt-3">
+      <h4>My Projects</h4>
+    </div>
+    <div class="col-md-4">
+      <div class="card-body">
+        <img src="/rb.png" class="card-img-top" alt="siajaib">
+        <div class="card-body">
+          <h5>Reformasi Birokrasi</h5>
+          <p>Website berita Pemkab Buleleng</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="card-body">
+        <img src="/siajaib.png" class="card-img-top" alt="siajaib">
+        <div class="card-body">
+          <h5>SiAjaib</h5>
+          <p>Sistem pengajuan perizinan online Kabupaten Buleleng</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="card-body">
+        <img src="/virendra.png" class="card-img-top" alt="siajaib">
+        <div class="card-body">
+          <h5>Wahana Virendra</h5>
+          <p>Sistem tiketing penyebrangan kapal Pelabuhan Padang Bai</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Profile.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="card bg-light" style="width: 18rem;">
+        <img src="@/assets/ADI WINATA.jpg" class="card-img-top" alt="pfp" id="pfp">
+        <div class="card-body">
+            <p>hello, I am</p>
+            <b class="card-text">Made Mas Adi Winata</b>
+            <p>
+                Software Engineer of Bimasakti Alterra
+            </p>
+        </div>
+    </div>
+</template>
